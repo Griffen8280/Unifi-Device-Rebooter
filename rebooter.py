f3142487d4b5bd0670dcc55b306a5e8083f4c37f
Chris P <42878642+Griffen8280@users.noreply.github.com>
2022-12-07 10:11:27 -0500

    Create rebooter.py
    added main program

---
+++ rebooter.py
@@ -0,0 +1,23 @@
+try:
+    import pexpect
+    from pexpect.popen_spawn import PopenSpawn
+except ModuleNotFoundError:
+    print("Module pexpect is not installed")
+    exit
+import time
+import shutil
+
+if shutil.which("plink") != None:
+    print("plink is installed")
+else:
+    print("plink is not installed")
+    print("run winget putty.putty to install it from terminal")
+    exit
+ssh_tunnel = pexpect.popen_spawn.PopenSpawn('plink ubnt@192.168.1.60 -pw ubnt -batch', encoding='utf-8')
+time.sleep(5)
+ssh_tunnel.sendline('reboot')
+time.sleep(5)
+ssh_tunnel.sendline('y')
+time.sleep (5)
+ssh_tunnel.expect(pexpect.EOF)
+exit
